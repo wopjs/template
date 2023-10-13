7344fc4822211f29ddd1140c4b77ade53cce9381
CRIMX <straybugs@gmail.com>
2023-10-13 21:23:19 +0800

    chore: add eslint import rules (#2)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,16 +5,82 @@ const config = {
     browser: true,
   },
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "import"],
   extends: [
     "eslint:recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
     "plugin:@typescript-eslint/recommended",
     "prettier",
   ],
   rules: {
-    "@typescript-eslint/consistent-type-imports": ["warn"],
+    "@typescript-eslint/consistent-type-imports": "error",
     "@typescript-eslint/no-empty-interface": "off",
-    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        argsIgnorePattern: "^_",
+        destructuredArrayIgnorePattern: "^_",
+        ignoreRestSiblings: true,
+      },
+    ],
+    "@typescript-eslint/ban-types": [
+      "error",
+      {
+        types: {
+          "{}": false,
+        },
+        extendDefaults: true,
+      },
+    ],
+    "import/no-unresolved": "off",
+    "import/newline-after-import": [
+      "error",
+      { considerComments: true, count: 1 },
+    ],
+    "import/consistent-type-specifier-style": ["error", "prefer-top-level"],
+    "import/no-duplicates": ["error", { considerQueryString: true }],
+    "import/order": [
+      "error",
+      {
+        "newlines-between": "always",
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+        groups: [
+          "object",
+          "type",
+          ["builtin", "external", "internal"],
+          ["parent", "sibling", "index"],
+        ],
+        pathGroups: [
+          {
+            pattern: "*.+(scss|css|less)",
+            patternOptions: { matchBase: true },
+            group: "object",
+          },
+          {
+            pattern: "~/**",
+            group: "internal",
+            position: "after",
+          },
+          {
+            pattern: "../**",
+            group: "parent",
+            position: "before",
+          },
+        ],
+        pathGroupsExcludedImportTypes: [
+          "builtin",
+          "external",
+          "object",
+          "type",
+        ],
+        distinctGroup: false,
+      },
+    ],
   },
 };
 
