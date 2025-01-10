bb8730d4584321b058bd4047e310237732e39ea1
CRIMX <straybugs@gmail.com>
2025-01-10 16:29:38 +0800

    chore: sort json

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,32 +2,46 @@ import jsEslint from "@eslint/js";
 import gitignore from "eslint-config-flat-gitignore";
 import eslintConfigPrettier from "eslint-config-prettier";
 import importX from "eslint-plugin-import-x";
+import jsonc from "eslint-plugin-jsonc";
 import perfectionist from "eslint-plugin-perfectionist";
 import tsEslint from "typescript-eslint";
 
 export default tsEslint.config(
-  jsEslint.configs.recommended,
-  ...tsEslint.configs.recommended,
-  eslintConfigPrettier,
-  importX.flatConfigs.recommended,
-  perfectionist.configs["recommended-natural"],
   gitignore(),
+  eslintConfigPrettier,
   {
+    extends: [
+      jsEslint.configs.recommended,
+      ...tsEslint.configs.recommended,
+      importX.flatConfigs.recommended,
+      perfectionist.configs["recommended-natural"],
+    ],
+    files: ["**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}"],
     languageOptions: {
       ecmaVersion: "latest",
       globals: {
         console: true,
         process: true,
       },
+      parser: tsEslint.parser,
       sourceType: "module",
     },
-    rules: {
-      "prefer-const": "off",
-      "sort-imports": "off",
+    plugins: {
+      "@typescript-eslint": tsEslint.plugin,
     },
-  },
-  {
     rules: {
+      "@typescript-eslint/consistent-type-imports": ["error", { fixStyle: "inline-type-imports" }],
+      "@typescript-eslint/no-empty-interface": "off",
+      "@typescript-eslint/no-non-null-assertion": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+          ignoreRestSiblings: true,
+          varsIgnorePattern: "^_",
+        },
+      ],
       "import-x/consistent-type-specifier-style": ["error", "prefer-inline"],
       "import-x/no-duplicates": ["error", { considerQueryString: true, "prefer-inline": true }],
       "import-x/no-unresolved": "off",
@@ -46,29 +60,26 @@ export default tsEslint.config(
           ],
         },
       ],
+      "prefer-const": "off",
+      "sort-imports": "off",
     },
   },
   {
-    files: ["**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}"],
-    languageOptions: {
-      parser: tsEslint.parser,
-    },
-    plugins: {
-      "@typescript-eslint": tsEslint.plugin,
-    },
+    extends: [...jsonc.configs["flat/recommended-with-json"]],
+    files: [
+      "src/**/*.json",
+      "scripts/**/*.json",
+      "ts-config.json",
+      ".prettierrc.json",
+      "typedoc.json",
+      "mangle-cache.json",
+    ],
     rules: {
-      "@typescript-eslint/consistent-type-imports": ["error", { fixStyle: "inline-type-imports" }],
-      "@typescript-eslint/no-empty-interface": "off",
-      "@typescript-eslint/no-non-null-assertion": "off",
-      "@typescript-eslint/no-unused-vars": [
-        "error",
-        {
-          argsIgnorePattern: "^_",
-          destructuredArrayIgnorePattern: "^_",
-          ignoreRestSiblings: true,
-          varsIgnorePattern: "^_",
-        },
-      ],
+      "jsonc/sort-keys": ["error", "asc", { caseSensitive: true, minKeys: 2, natural: true }],
     },
+  },
+  {
+    extends: [...jsonc.configs["flat/recommended-with-jsonc"]],
+    files: ["**/*.jsonc", ".vscode/*.json"],
   },
 );
